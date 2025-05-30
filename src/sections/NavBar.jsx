8ebee41e94d186f689e32e4f71c36cca7e79530e
aaronersando <agersando@gmail.com>
2025-05-30 10:37:15 +0800

    Navbar component section setup done

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "motion/react"
 
 function Navigation(){
     return(
@@ -25,8 +28,34 @@ function NavBar(){
     const [isOpen, setIsOpen] = useState(false);
 
     return(
-        <div className="fixed inset-x-0">
+        <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
+            <div className="mx-auto c-space max-w-7xl">
+                <div className="flex items-center justify-between py-2 sm:py-0">
+                    <a href="/" className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">
+                    Aaron
+                    </a>
+                    <button 
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden">
+                        <FontAwesomeIcon className="w-6 h-6" icon={faBars} />
+                    </button>
+                    <nav className="hidden sm:flex">
+                        <Navigation/>
+                    </nav>
+                </div>
+            </div>
+            {isOpen && (<motion.div 
+            className="block overflow-hidden text-center sm:hidden"
+            initial={{ opacity: 0,  x:10}}
+            animate={{ opacity: 1, x: 0}}
+            style={{ maxHeight: "100vh"}}
+            transition={{ duration: 1}}
+            >
+                <nav className="pb-5">
+                    <Navigation/>
+                </nav>
 
+            </motion.div>)}
         </div>
     )
 }
